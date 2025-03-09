03da2f1b16a566ea5afccb2b48b9cbaa94b3c20b
Hoang-Thang Ta <37979473+hoangthangta@users.noreply.github.com>
2025-03-09 10:38:08 +0700

    Add files via upload

--- models/__init__.py
+++ models/__init__.py
@@ -10,9 +10,15 @@ from .prkan import PRKAN
 
 from .relu_kan import ReLUKAN
 from .af_kan import AF_KAN
+from .cheby_kan import ChebyKAN
+from .fourier_kan import FourierKAN
+
+from .knots_kan import KnotsKAN
+from .rational_kan import RationalKAN
+from .rbf_kan import RBF_KAN
 
 #from .functions import lsin, lcos, larctan, lrelu, lleaky_relu, lswish, lmish, lsoftplus, lhard_sigmoid, lelu, lshifted_softplus, lgelup
 # "lsin", "lcos", "larctan", "lrelu", "lleaky_relu", "lswish", "lmish", "lsoftplus", "lhard_sigmoid", "lelu", "lshifted_softplus", "lgelup"
 
-__all__ = ["EfficientKAN", "EfficientKANLinear", "FastKAN", "FasterKAN", "BSRBF_KAN", "GottliebKAN", "MLP", "FC_KAN", "SKAN", "PRKAN", "ReLUKAN", "AF_KAN"]
+__all__ = ["EfficientKAN", "EfficientKANLinear", "FastKAN", "FasterKAN", "BSRBF_KAN", "GottliebKAN", "MLP", "FC_KAN", "SKAN", "PRKAN", "ReLUKAN", "AF_KAN", "ChebyKAN", "KnotsKAN", "FourierKAN", "RationalKAN", "RBF_KAN"]
 
